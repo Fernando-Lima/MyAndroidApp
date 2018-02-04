ed8ea70551847be6e11ea808e6097fc72abffe09
Fernando <fernandosupweb@gmail.com>
2018-02-04 21:59:16 -0200

    Adicionado ViewPager para elementos do navigation bottom

--- app/src/main/java/com/example/fernando/myapplication/CadGrupoActivity.java
+++ app/src/main/java/com/example/fernando/myapplication/CadGrupoActivity.java
@@ -3,13 +3,77 @@ package com.example.fernando.myapplication;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CadGrupoActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    private BottomNavigationView navigation;
+    private ViewPager viewPager;
+    private List<View> viewList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cad_grupo);
+
+        initView();
+    }
+
+    private void initView(){
+        View view1 = getLayoutInflater().inflate(R.layout.item_view_pager_1,null);
+        View view2 = getLayoutInflater().inflate(R.layout.item_view_pager_2,null);
+        View view3 = getLayoutInflater().inflate(R.layout.item_view_pager_3,null);
+
+        viewList = new ArrayList<>();
+        viewList.add(view1);
+        viewList.add(view2);
+        viewList.add(view3);
+
+        viewPager = findViewById(R.id.view_pager_bottom_navigation);
+        viewPager.setAdapter(pagerAdapter);
+        viewPager.addOnPageChangeListener(pageChangeListener);
+
+
+        navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+    }
+
+    private ViewPager.OnPageChangeListener pageChangeListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            switch (position) {
+                case 0:
+                    navigation.setSelectedItemId(R.id.navigation_home);
+                    break;
+                case 1:
+                    navigation.setSelectedItemId(R.id.navigation_dashboard);
+                    break;
+                case 2:
+                    navigation.setSelectedItemId(R.id.navigation_notifications);
+                    break;
+            }
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -18,27 +82,42 @@ public class CadGrupoActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_user);
+                    viewPager.setCurrentItem(0);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_config);
+                    viewPager.setCurrentItem(1);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_regra);
+                    viewPager.setCurrentItem(2);
                     return true;
             }
             return false;
         }
     };
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cad_grupo);
+    private PagerAdapter pagerAdapter = new PagerAdapter() {
+        @Override
+        public int getCount() {
+            return viewList.size();
+        }
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-    }
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView(viewList.get(position));
+            Log.i("ciclo","destruiu item view");
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            container.addView(viewList.get(position));
+            Log.i("ciclo","add item view");
+            return viewList.get(position);
+        }
+    };
 
 }
